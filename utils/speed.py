c9184500c7a8285793d3711baee7aa4f49144ea7
ldm2060 <ldm200026@live.cn>
2025-03-18 21:15:57 +0800

    fix: 修复速度计算逻辑，确保处理None值以避免错误

--- utils/speed.py
+++ utils/speed.py
@@ -358,13 +358,16 @@ def sort_urls(name, data, supply=config.open_supply, filter_speed=config.open_fi
             cache_list = cache[host]
             if cache_list:
                 cache_list_nonezero = [
-                    item for item in cache_list if (item['speed'] > 0)]
+                    item for item in cache_list if (item['speed'] is not None and item['speed'] > 0)]
                 if len(cache_list_nonezero) != 0:
                     avg_speed: int | float | None = sum(
                         item['speed'] for item in cache_list_nonezero) / len(cache_list_nonezero)
                 else :
                     avg_speed = float("inf")
-                avg_speed = max(avg_speed, speed)
+                if avg_speed is not None and speed is not None:
+                    avg_speed = max(avg_speed, speed)
+                elif speed is not None:
+                    avg_speed = speed
                 avg_delay: int | float | None = max(
                     int(sum(item['delay'] or -1 for item in cache_list) / len(cache_list)), -1)
                 # resolution = max((item['resolution'] for item in cache_list), key=get_resolution_value) or resolution
